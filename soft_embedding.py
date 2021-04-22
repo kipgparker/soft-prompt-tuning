8c763544e02ad854692e92030d5f4c478fcc5b01
kip <kipgparker@gmail.com>
2021-04-23 00:32:01 +0100

    flesh out embedding code

--- soft_embedding.py
+++ soft_embedding.py
@@ -3,33 +3,51 @@ import torch.nn as nn
 
 class SoftEmbedding(nn.Module):
     def __init__(self, 
-                embedding_dimension: int,
-                num_tokens: int = 20, 
+                wte: nn.Embedding,
+                n_tokens: int = 10, 
                 random_range: float = 0.5,
                 initialize_from_vocab: bool = True):
-        """[summary]
+        """appends learned embedding to 
 
         Args:
-            embedding_dimension (int): [description]
-            num_tokens (int, optional): [description]. Defaults to 20.
-            random_range (float, optional): [description]. Defaults to 0.5.
+            wte (nn.Embedding): original transformer word embedding
+            n_tokens (int, optional): number of tokens for task. Defaults to 10.
+            random_range (float, optional): range to init embedding (if not initialize from vocab). Defaults to 0.5.
+            initialize_from_vocab (bool, optional): initalizes from default vocab. Defaults to True.
         """
-        super(Net, self).__init__()
-        self.initialize_from_vocab = initialize_from_vocab
-        self.wte = None
-        self.embedding = nn.parameter.Parameter(torch.FloatTensor(embedding_dimension, num_tokens).uniform_(-random_range, random_range))
-
-    def initialize_embedding(self):
-        """Initalizes soft embedding to random values from input WTE emedding
-        """
-        pass
-
-    def set_input_embeddings(self, new_embeddings: nn.Embedding):
-        """[summary]
+        super(SoftEmbedding, self).__init__()
+        self.wte = wte
+        self.learned_embedding = nn.parameter.Parameter(self.initialize_embedding(wte,
+                                                                               n_tokens, 
+                                                                               random_range, 
+                                                                               initialize_from_vocab))
+            
+    def initialize_embedding(self, 
+                             wte: nn.Embedding,
+                             n_tokens: int = 10, 
+                             random_range: float = 0.5, 
+                             initialize_from_vocab: bool = True):
+        """initializes learned embedding
 
         Args:
-            new_embeddings (nn.Embedding): [existing WTE embedding within the language model]
+            same as __init__
+
+        Returns:
+            torch.float: initialized using original schemes
         """
-        self.wte = new_embeddings
-        if self.initialize_from_vocab:
-            initialize_embedding()
+        if initialize_from_vocab:
+            return self.wte.weight[:n_tokens].clone().detach()
+        return torch.FloatTensor(wte.weight.size(1), n_tokens).uniform_(-random_range, random_range)
+            
+    def forward(self, tokens):
+        """run forward pass
+
+        Args:
+            tokens (torch.long): input tokens before encoding
+
+        Returns:
+            torch.float: encoding of text concatenated with learned task specifc embedding
+        """
+        input_embedding = self.wte(tokens)
+        learned_embedding = self.learned_embedding.repeat(input_embedding.size(0), 1, 1)
+        return torch.cat([learned_embedding, input_embedding], 1)
